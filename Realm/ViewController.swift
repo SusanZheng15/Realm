4a4d2a110dc6ef6ee79c22ddebfa01b14db192f5
SusanZheng15 <zheng.susan12@gmail.com>
2017-04-23 09:46:51 -0400

    first commit

---
+++ Realm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realm
+//
+//  Created by Flatiron School on 4/23/17.
+//  Copyright © 2017 Susan Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
